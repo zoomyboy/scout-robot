f623d36b578e4e65ba170e693848cd88431cd2cf
Philipp Lang <pille@zoomyboy.de>
2017-08-08 00:28:05 +0200

    Fixed: Anrede in login Wurde komplett auf "du" geändert, und "Gebe" anstatt "Gib" Bitte weiterhin so verfahren!

--- resources/assets/js/components/login/login.vue
+++ resources/assets/js/components/login/login.vue
@@ -1,11 +1,11 @@
 <template>
 	<div>
 		<heading title="Einloggen"></heading>
-		Herzlich Willkommen bei Scout Robot. Bitte gib deine Logindaten ein.
+		Herzlich Willkommen bei Scout Robot. Bitte gebe deine Logindaten ein.
 		<br><br>
 		<vf-form action="/login" method="post" ajax follow>
-			<vf-text name="email" label="E-Mail-Adresse" help="Gib hier die E-Mail-Adresse deines Zugangs ein."></vf-text>
-			<vf-password name="password" label="Passwort"  help="Gib hier das Passwort deines Zugangs ein."></vf-password>
+			<vf-text name="email" label="E-Mail-Adresse" help="Gebe hier die E-Mail-Adresse deines Zugangs ein."></vf-text>
+			<vf-password name="password" label="Passwort"  help="Gebe hier das Passwort deines Zugangs ein."></vf-password>
 			<vf-submit>Einloggen</vf-submit>
 			<v-link route="newpw">Passwort vergessen</v-link>
 		</vf-form>
--- resources/assets/js/components/login/new-password.vue
+++ resources/assets/js/components/login/new-password.vue
@@ -1,10 +1,10 @@
 <template>
 	<div>
 		<heading title="Neues Passwort erstellen"></heading>
-		Geben Sie hier die E-Mail-Adresse Ihres Zugangs ein. Daraufhin werden wir eine E-Mail an diese Adresse senden, wo Sie Ihr Passwort zurücksetzen können.
+		Gebe hier die E-Mail-Adresse deines Zugangs ein. Daraufhin werden wir eine E-Mail an diese Adresse senden, wo du dein Passwort zurücksetzen kannst.
 		<br><br>
 		<vf-form action="/password/email" redirect="login" method="post" msg="E-Mail wurde erfolgreich versendet." ajax>
-			<vf-text name="email" label="E-Mail-Adresse" help="Die E-Mail-Adresse Ihres Zugangs"></vf-text>
+			<vf-text name="email" label="E-Mail-Adresse" help="Die E-Mail-Adresse deines Zugangs"></vf-text>
 			<vf-submit>Passwort anfordern</vf-submit>
 			<v-link route="login">Zurück</v-link>
 		</vf-form>
