c523e14c0a15ee1f239df3b839da6e9f2eac6278
Philipp Lang <pille@zoomyboy.de>
2018-02-01 21:28:20 +0100

    Add container to login form

--- resources/assets/js/components/login/login.vue
+++ resources/assets/js/components/login/login.vue
@@ -1,37 +1,16 @@
 <template>
+	<v-container>
     <v-card color="grey lighten-4">
         <v-form v-model="valid">
             <v-subheader>Herzlich Willkommen bei Scout Robot. Bitte gebe deine Logindaten ein.</v-subheader>
             <v-container fluid>
-                <v-layout row>
-                    <v-flex xs4>
-                        <v-subheader>E-Mail-Adresse</v-subheader>
-                    </v-flex>
-                    <v-flex xs8>
-                        <v-text-field name="email" label="E-Mail-Adresse" id="email" required :rules="[validateEmail()]" v-model="values.email"></v-text-field>
-                    </v-flex>
-                </v-layout>
-                <v-layout row>
-                    <v-flex xs4>
-                        <v-subheader>Passwort</v-subheader>
-                    </v-flex>
-                    <v-flex xs8>
-                        <v-text-field name="password" label="Passwort" id="password" required :rules="[validateLength()]" v-model="values.password" type="password"></v-text-field>
-                    </v-flex>
-                </v-layout>
+                <v-text-field name="email" label="E-Mail-Adresse" id="email" required :rules="[validateEmail()]" v-model="values.email"></v-text-field>
+                <v-text-field name="password" label="Passwort" id="password" required :rules="[validateLength()]" v-model="values.password" type="password"></v-text-field>
             </v-container>
             <v-btn :disabled="!valid" @click="submit" color="primary">Absenden</v-btn>
         </v-form>
     </v-card>
-	<!-- <div>
-		<br><br>
-		<vf-form action="/login" method="post" follow>
-			<vf-text name="email" label="E-Mail-Adresse" help="Gebe hier die E-Mail-Adresse deines Zugangs ein."></vf-text>
-			<vf-password name="password" label="Passwort"  help="Gebe hier das Passwort deines Zugangs ein."></vf-password>
-			<vf-submit>Einloggen</vf-submit>
-			<v-link route="newpw">Passwort vergessen</v-link>
-		</vf-form>
-    </div> -->
+	</v-container>
 </template>
 
 <script>
