d500df7c9ecb8ed6d69209480a8808d51422a40f
Philipp Lang <pille@zoomyboy.de>
2018-02-17 22:32:01 +0100

    Fixed login redirect

--- resources/assets/js/components/login/login.vue
+++ resources/assets/js/components/login/login.vue
@@ -1,13 +1,13 @@
 <template>
 	<v-container>
     <v-card color="grey lighten-4">
-        <v-form v-model="valid">
+        <v-form v-model="valid" method="post" @submit.prevent="submit">
             <v-subheader>Herzlich Willkommen bei Scout Robot. Bitte gebe deine Logindaten ein.</v-subheader>
             <v-container fluid>
                 <v-text-field name="email" label="E-Mail-Adresse" id="email" required :rules="[validateEmail()]" v-model="values.email"></v-text-field>
                 <v-text-field name="password" label="Passwort" id="password" required :rules="[validateRequired()]" v-model="values.password" type="password"></v-text-field>
             </v-container>
-            <v-btn :disabled="!valid" @click="submit" color="primary">Absenden</v-btn>
+            <v-btn :disabled="!valid" type="submit" color="primary">Absenden</v-btn>
         </v-form>
     </v-card>
 	</v-container>
@@ -29,7 +29,7 @@
                 var vm = this;
     
                 axios.post('/login', this.values).then((ret) => {
-                    window.locatin.href = '/';
+                    window.location.href = '/';
                 }).catch((error) => {
                     var field = Object.keys(error.response.data)[0];
                     var msg = error.response.data[Object.keys(error.response.data)[0]];
