96789b9f0dcf13261f2f55245bb2fe3f26eb6691
stammsilva root <webmaster@stammsilva.de>
2018-09-04 22:28:42 +0200

    Mod: Payment should have foreign keys also fillable

--- app/Payment.php
+++ app/Payment.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    public $fillable = ['nr'];
+    public $fillable = ['nr', 'subscription_id', 'status_id', 'member_id'];
 
 	public $casts = [
 		'amount' => 'int'
