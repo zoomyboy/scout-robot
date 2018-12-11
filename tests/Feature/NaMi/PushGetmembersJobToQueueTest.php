fdefb7793fa7ed3331cfdc480b1e710cfa66c212
Philipp Lang <pille@zoomyboy.de>
2018-12-11 02:13:35 +0100

    WIP

--- tests/Feature/NaMi/PushGetmembersJobToQueueTest.php
+++ tests/Feature/NaMi/PushGetmembersJobToQueueTest.php
@@ -2,12 +2,13 @@
 
 namespace Tests\Feature\NaMi;
 
+use Setting;
+use App\Member;
+use Tests\FeatureTestCase;
 use App\Facades\NaMi\NaMiMember;
 use App\Jobs\SyncAllNamiMembers;
-use App\Member;
+use App\Events\Import\MemberCreated;
 use Illuminate\Support\Facades\Queue;
-use Tests\FeatureTestCase;
-use Setting;
 
 class PushGetmembersJobToQueueTest extends FeatureTestCase {
 	public $config;
@@ -20,7 +21,6 @@ class PushGetmembersJobToQueueTest extends FeatureTestCase {
 		$this->runSeeder('CountrySeeder');
 		$this->runSeeder('ConfSeeder');
 
-        Queue::fake();
         $this->authAsApi();
 	}
 
@@ -28,11 +28,22 @@ class PushGetmembersJobToQueueTest extends FeatureTestCase {
 	public function it_synchs_active_and_inactive_members() {
 		Setting::set('namiEnabled', true);
 
+        $this->fakeOnlineNamiMembers([
+            ['id' => 2334, 'status' => 'Aktiv'],
+            ['id' => 2335, 'status' => 'Inaktiv']
+        ]);
+
 		$this->postApi('nami/getmembers', ['active' => true, 'inactive' => true])
 			->assertSuccess();
 
-		Queue::assertPushed(SyncAllNamiMembers::class, function($q) {
-            return $q->filter['status'] == ['Aktiv', 'Inaktiv'];
+        $this->assertDatabaseHas('members', ['nami_id' => 2334]);
+        $this->assertDatabaseHas('members', ['nami_id' => 2335]);
+
+        Event::assertDispatched(MemberCreated::class, function($e) {
+            return $e->progress == 50 && $e->member->nami_id == 2334;
+        });
+        Event::assertDispatched(MemberCreated::class, function($e) {
+            return $e->progress == 100 && $e->member->nami_id == 2335;
         });
 	}
 
@@ -40,11 +51,22 @@ class PushGetmembersJobToQueueTest extends FeatureTestCase {
     public function it_synchs_active_members() {
         Setting::set('namiEnabled', true);
 
+        $this->fakeOnlineNamiMembers([
+            ['id' => 2334, 'status' => 'Aktiv'],
+            ['id' => 2335, 'status' => 'Inaktiv']
+        ]);
+
         $this->postApi('nami/getmembers', ['active' => true, 'inactive' => false])
             ->assertSuccess();
 
-        Queue::assertPushed(SyncAllNamiMembers::class, function($q) {
-            return $q->filter['status'] == ['Aktiv'];
+        $this->assertDatabaseHas('members', ['nami_id' => 2334]);
+        $this->assertDatabaseMissing('members', ['nami_id' => 2335]);
+
+        Event::assertDispatched(MemberCreated::class, function($e) {
+            return $e->progress == 100 && $e->member->nami_id == 2334;
+        });
+        Event::assertNotDispatched(MemberCreated::class, function($e) {
+            return $e->member->nami_id == 2335;
         });
     }
 
@@ -52,11 +74,41 @@ class PushGetmembersJobToQueueTest extends FeatureTestCase {
     public function it_synchs_inactive_members() {
         Setting::set('namiEnabled', true);
 
+        $this->fakeOnlineNamiMembers([
+            ['id' => 2334, 'status' => 'Aktiv'],
+            ['id' => 2335, 'status' => 'Inaktiv']
+        ]);
+
         $this->postApi('nami/getmembers', ['active' => false, 'inactive' => true])
             ->assertSuccess();
 
-        Queue::assertPushed(SyncAllNamiMembers::class, function($q) {
-            return $q->filter['status'] == ['Inaktiv'];
+        $this->assertDatabaseMissing('members', ['nami_id' => 2334]);
+        $this->assertDatabaseHas('members', ['nami_id' => 2335]);
+
+        Event::assertDispatched(MemberCreated::class, function($e) {
+            return $e->progress == 100 && $e->member->nami_id == 2335;
+        });
+        Event::assertNotDispatched(MemberCreated::class, function($e) {
+            return $e->member->nami_id == 2334;
+        });
+    }
+
+    /** @test */
+    public function it_updates_a_local_nami_member() {
+        Setting::set('namiEnabled', true);
+
+        $this->fakeOnlineNamiMembers([
+            ['id' => 2334, 'status' => 'Aktiv', 'vorname' => 'Philipp']
+        ]);
+        $this->create('Member', ['nami_id' => 2334, 'firstname' => 'Max']);
+
+        $this->postApi('nami/getmembers', ['active' => true, 'inactive' => true])
+            ->assertSuccess();
+
+        $this->assertDatabaseHas('members', ['nami_id' => 2334, 'firstname' => 'Philipp']);
+
+        Event::assertNotDispatched(MemberUpdated::class, function($e) {
+            return $e->member->nami_id == 2334 && $e->member->firstname == 'Philipp';
         });
     }
 
@@ -64,23 +116,35 @@ class PushGetmembersJobToQueueTest extends FeatureTestCase {
     public function it_synchs_no_members_without_any_filter() {
         Setting::set('namiEnabled', true);
 
+        $this->fakeOnlineNamiMembers([
+            ['id' => 2334, 'status' => 'Aktiv'],
+            ['id' => 2335, 'status' => 'Inaktiv']
+        ]);
+
         $this->postApi('nami/getmembers', ['active' => false, 'inactive' => false])
             ->assertSuccess();
 
-        Queue::assertPushed(SyncAllNamiMembers::class, function($q) {
-            return $q->filter['status'] == [];
-        });
+        $this->assertDatabaseMissing('members', ['nami_id' => 2334]);
+        $this->assertDatabaseMissing('members', ['nami_id' => 2335]);
+
+        Event::assertNotDispatched(MemberCreated::class);
     }
 
 	/** @test */
 	public function it_doesnt_sync_when_nami_is_not_enabled() {
-		$this->withExceptionHandling();
-
         Setting::set('namiEnabled', false);
 
-		$this->postApi('nami/getmembers', ['active' => true, 'inactive' => true])
-			->assertValidationFailedWith('error');
+        $this->fakeOnlineNamiMembers([
+            ['id' => 2334, 'status' => 'Aktiv'],
+            ['id' => 2335, 'status' => 'Inaktiv']
+        ]);
 
-		Queue::assertNotPushed(SyncAllNaMiMembers::class);
+        $this->postApi('nami/getmembers', ['active' => false, 'inactive' => false])
+            ->assertSuccess();
+
+        $this->assertDatabaseMissing('members', ['nami_id' => 2334]);
+        $this->assertDatabaseMissing('members', ['nami_id' => 2335]);
+
+        Event::assertNotDispatched(MemberCreated::class);
 	}
 }
