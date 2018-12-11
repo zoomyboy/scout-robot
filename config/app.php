1d1f934a295f2aab3dda9d2d7eaa82594426ec74
root <root@zoomyboy.de>
2018-12-11 01:30:41 +0100

    Add Laravel Horizon

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\PdfServiceProvider::class,
 		Laravel\Passport\PassportServiceProvider::class,
